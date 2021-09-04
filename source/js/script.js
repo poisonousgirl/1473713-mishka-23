acf960352c5d188fb3fec4c622fff6201e570615
Anna <gravillon.anna@gmail.com>
2021-09-04 15:28:22 +0200

    Декоративные элементы главной страницы Не работающая шапка

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,12 @@
-let button = document.querySelector('.main-nav__toggle');
-let modal = document.querySelector('.main-nav__list');
-modal.classList.add('main-nav__list--hidden');
+var button = document.querySelector('.main-nav__toggle');
+var menu = document.querySelector('.main-nav__list');
+var header = document.querySelector('.page-header')
+
+header.classList.remove('page-header--nojs');
+
+menu.classList.add('main-nav__list--hidden');
+button.classList.remove('main-nav__toggle--opened')
 button.onclick = function() {
-    modal.classList.toggle('main-nav__list--hidden');
+  button.classList.toggle('main-nav__toggle--opened')
+  menu.classList.toggle('main-nav__list--hidden');
 }
